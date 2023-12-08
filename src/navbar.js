41489052e8ac37be03cf1d426db47206a6af1b86
idwessough <sough.idwes@gmail.com>
2023-12-08 12:04:51 -0500

    Navbar Styling with smooth and blurry effects

--- src/navbar.js
+++ src/navbar.js
@@ -28,6 +28,41 @@ fetch('navbar.html')
     document.getElementById('navbar').innerHTML = data;
   });
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    let lastScrollTop = 0;
+    const navbar = document.querySelector('.navbar');
+
+    console.log("Launch Navbar.js");
+    const navbarHeight = navbar.offsetHeight; // Get the dynamic height of the navbar
+
+    window.addEventListener('scroll', function() {
+        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        if (currentScroll > lastScrollTop) {
+            // Scrolling down
+            navbar.style.top = `-${navbarHeight}px`; // Use navbarHeight here
+        } else {
+            // Scrolling up
+            navbar.style.top = '0px';
+        }
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+    }, false);
+}); 
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const navbar = document.querySelector('.navbar');
+    const bodyBefore = document.createElement('div');
+    bodyBefore.id = 'body-before';
+    document.body.insertBefore(bodyBefore, document.body.firstChild);
+
+    function adjustPseudoElementHeight() {
+        const navbarHeight = navbar.offsetHeight;
+        bodyBefore.style.height = navbarHeight + 'px';
+    }
+
+    window.addEventListener('resize', adjustPseudoElementHeight);
+    adjustPseudoElementHeight(); // Initial adjustment
+});
+
 
 
 // // Path: src\footer.js
